473cdcf9461b801eb5baacb657017fda59765b31
nileshivam <dwivedinilesh11@gmail.com>
2017-03-29 13:58:59 +0530

    test-commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dwivedi Nilesh
                 </div>
 
                 <div class="links">
